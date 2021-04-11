e668224bc31e1a94f481875005fdd7b4ce24f552
Valeria Terziyska <valeria.terziyska@gmail.com>
2021-04-11 18:43:01 +0300

    refactor authorization middleware

--- middlewares/isAuthorize.js
+++ middlewares/isAuthorize.js
@@ -1,14 +1,7 @@
-const { SECRET, COOKIE_NAME } = require("../config/config");
-const jwt = require("jsonwebtoken");
-
 module.exports = function (req, res, next) {
-    let token = req.cookies[COOKIE_NAME];
+    if(!req.user) {
+        return res.redirect("/auth/login");
+    }
 
-    if (!token) return res.redirect("/auth/login");
-
-    jwt.verify(token, SECRET, function (err, decoded) {
-        if (err) return res.redirect("/auth/login");
-
-        next();
-    });
+    next();
 };
